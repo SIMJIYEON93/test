f45ed9bbcc581b3bcb161441355c6b7c71bae577
SIMJIYEON93 <930515s@gmail.com>
2025-01-16 21:14:23 +0900

    init

--- src/main/java/com/example/test/WebSecurityConfig.java
+++ src/main/java/com/example/test/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.test;
+/*package com.example.test;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,3 +18,4 @@ public class WebSecurityConfig {
         return http.build();
     }
 }
+*/
